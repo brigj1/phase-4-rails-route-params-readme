ad634fc87bcc13450aae9cc9bc22d191a171f973
Brigham <brig.johnson@gmail.com>
2022-06-30 11:33:45 -0400

    commit tree 9123ebc75fa4d833922b76dcd9fb755e53b6e735

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,16 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #byebug
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
+
+  def dum
+    #byebug
+    #cheese = Cheese.find(params[:id])
+    blah = "extra param is #{params[:dummy]}"
+    render json: blah
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+  get '/cheeses/:id', to: 'cheeses#show'
+  get '/cheeses/:id/:dummy', to: 'cheeses#dum'
 end
